aa7f3c15f7494a00a128560cf261b51b61ee7627
Sankethlreddy <132805025+Sankethlreddy@users.noreply.github.com>
2024-02-22 11:30:31 +0530

    Updated expenses

--- Home/models.py
+++ Home/models.py
@@ -1,11 +1,12 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
 class Expenses(models.Model):
-    expense_id = models.IntegerField(primary_key=True)
-    id = models.ForeignKey(User, on_delete=models.CASCADE)
+    expense_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     category_name = models.CharField(max_length=10)
     amount = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     description = models.CharField(max_length=255, null=True)
@@ -13,17 +14,17 @@ class Expenses(models.Model):
     method_name = models.CharField(max_length=10, null=True)
 
     def _str_(self):
-        return f"Expense {self.expense_id} by {self.User.id}"
+        return f"Expense {self.expense_id} by {self.user.username}"
 
 class Payment_Method(models.Model):
     payment_id = models.AutoField(primary_key=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     method_name = models.CharField(max_length=10, null=True)
-    id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     expense = models.ForeignKey(Expenses, on_delete=models.CASCADE, null=True)
 
     def _str_(self):
-        return f"Expense {self.payment_id} by {self.User.id}"
+        return f"Expense {self.payment_id} by {self.user.username}"
 
 class Category(models.Model):
     expense_id = models.ForeignKey(Expenses, on_delete=models.CASCADE)
@@ -35,9 +36,9 @@ class Budget(models.Model):
     budget_id = models.AutoField(primary_key=True)
     monthly_limit = models.IntegerField(null=True)
     category_name = models.CharField(max_length=10, null=True)
-    id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def _str_(self):
-        return f"Expense {self.budget_id} by {self.User.id}"
+        return f"Expense {self.budget_id} by {self.user.username}"
 
 
 
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+import uuid
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+
+from Home import models
 
 from .forms import Dashboard
 
@@ -7,12 +10,19 @@ def Home(request):
     return render(request,'Home/Home.html')
 
 def Expense_Tracking(request):
-    form = Dashboard(request.POST)
     if request.method == "POST":
-        form.save
-        if form.is_valid():
-            Amount = form.cleaned_data.get('Amount')
-    return render(request,'Home/index.html',{'form': form})
+        user_instance  = request.user
+        expense_id = str(uuid.uuid4())
+        Amount = request.POST['expense-amount']
+        category = request.POST['category']
+        Date = request.POST['expense-date']
+        Payment_method = request.POST['method']
+        description = request.POST['expense-note']
+        user_expenses = models.Expenses(user = user_instance ,amount = Amount ,expense_id =expense_id, category_name=category , expense_date = Date , method_name=Payment_method,description=description)
+        user_expenses.save()
+        return redirect('Expense-Tracking')
+    return render(request,'Home/index.html')
+
 
 def Budget(request):
     return render(request,'Home/Budget.html')
